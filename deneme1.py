ac12762669be268bb9e696aef7c4cc306a9a92c3
erhan3861 <erhanduyku@gmail.com>
2021-11-21 20:37:21 +0300

    icon

--- deneme1.py
+++ deneme1.py
@@ -6,12 +6,21 @@ pygame.init()
 # create the screen
 screen = pygame.display.set_mode((800, 600))
 
-running = True
+# Title and icon
+pygame.display.set_caption("Space Invaders")
 
+# https://www.flaticon.com/search?word=space&type=icon
+icon = pygame.image.load("spaceship.png")
+pygame.display.set_icon(icon)
+
+# Game Loop
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+
 
 
 
