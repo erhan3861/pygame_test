387e4548bedca2a5d17252065971564b6fc00ea1
erhan3861 <erhanduyku@gmail.com>
2021-11-21 20:54:30 +0300

    ship

--- deneme1.py
+++ deneme1.py
@@ -13,14 +13,27 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("spaceship.png")
 pygame.display.set_icon(icon)
 
+# Player
+playerImg = pygame.image.load("img.png")
+playerImg = pygame.transform.scale(playerImg, (64, 64))
+playerX = 370
+playerY = 480
+
+
+# blit means draw
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+
 # Game Loop
 running = True
 while running:
+    # RGB red,Green,Blue
+    screen.fill((100, 10, 200))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-
-
-
-
+    player()
+    pygame.display.update()
